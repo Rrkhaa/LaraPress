3f5382a0d3bb5586ad8c3057eff10aae21957ba2
Rrkhaa <rakha4523088@univpancasila.ac.id>
2025-10-03 10:49:01 +0700

    Initial commit dan menambahkan halaman kontak

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontak Kami</title>
+</head>
+<body>
+    <h1>Hubungi Kami</h1>
+    
+    
+    <hr>
+
+    <h2>Informasi Kontak Fiktif</h2>
+    <p>Silakan hubungi tim kami untuk pertanyaan atau kerja sama.</p>
+    
+    <ul>
+        <li><strong>Email:</strong> halo@rakhaabhirama.com</li>
+        <li><strong>Telepon:</strong> +62 812-3456-7890</li>
+        <li><strong>Alamat Kantor:</strong> Jl. Fiktif No. 42, Kota Koding, 12345</li>
+    </ul>
+
+</body>
+</html>
